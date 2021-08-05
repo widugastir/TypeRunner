be66ab7e1baa584be27b94ea02476439adf01515
M <sandsteppe@gmail.com>
2021-08-05 21:15:28 +0300

    fix 18

--- Assets/TypeRunner/Mechanics/Map Generation/Scripts/StickmanKiller.cs
+++ Assets/TypeRunner/Mechanics/Map Generation/Scripts/StickmanKiller.cs
@@ -4,12 +4,14 @@ namespace TypeRunner
 {
 	public class StickmanKiller : MonoBehaviour
 	{
+		[SerializeField] private bool _killOnlyBlocked = true;
+		
 		//------METHODS
 		private void OnTriggerEnter(Collider other)
 		{
 			if(other.TryGetComponent(out Mankin man))
 			{
-				if(man.Commands._isBlocked)
+				if(_killOnlyBlocked && man.Commands._isBlocked || _killOnlyBlocked == false)
 				{
 					man.Kill();
 				}
