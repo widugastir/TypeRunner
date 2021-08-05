33ae340dbef660675fd2fed900f1e7c53eb3e8a2
M <sandsteppe@gmail.com>
2021-08-05 19:29:24 +0300

    fix 16

---
+++ Assets/TypeRunner/Mechanics/Map Generation/Scripts/StickmanKiller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TypeRunner
+{
+	public class StickmanKiller : MonoBehaviour
+	{
+		//------METHODS
+		private void OnTriggerEnter(Collider other)
+		{
+			if(other.TryGetComponent(out Mankin man))
+			{
+				if(man.Commands._isBlocked)
+				{
+					man.Kill();
+				}
+			}
+		}
+	}
+}
