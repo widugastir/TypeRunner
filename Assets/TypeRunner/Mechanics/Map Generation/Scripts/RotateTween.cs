26f0d205e298339a2af2046f014bb58b6bc6b3bd
SoundSteppe <65368143+widugastir@users.noreply.github.com>
2021-08-17 03:45:21 +0300

    заменена анимация вращения монет на rotateTween

---
+++ Assets/TypeRunner/Mechanics/Map Generation/Scripts/RotateTween.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTween : MonoBehaviour
+{
+	[SerializeField] private Vector3 _rotateSpeed;
+	
+	private void Update()
+    {
+	    transform.Rotate(_rotateSpeed * Time.unscaledDeltaTime);
+    }
+}
