4640292cc2b19ff850a7034b666dd8a4d3e3d328
SoundSteppe <65368143+widugastir@users.noreply.github.com>
2021-08-17 03:45:21 +0300

    заменена анимация вращения монет на rotateTween

---
+++ Assets/TypeRunner/Mechanics/Manikins/Prefabs/TestMeshSimplifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMeshSimplifier : MonoBehaviour
+{
+	void OnGUI()
+	{
+		if (GUI.Button(Rect.MinMaxRect(0, 0, 200, 200),"Simplify Mesh"))
+		{
+			var originalMesh = GetComponent<MeshFilter>().sharedMesh;
+			float quality = 0.2f;
+			var meshSimplifier = new UnityMeshSimplifier.MeshSimplifier();
+			meshSimplifier.Initialize(originalMesh);
+			meshSimplifier.SimplifyMesh(quality);
+			var destMesh = meshSimplifier.ToMesh();
+			GetComponent<MeshFilter>().sharedMesh = destMesh;
+		}
+	}
+}
